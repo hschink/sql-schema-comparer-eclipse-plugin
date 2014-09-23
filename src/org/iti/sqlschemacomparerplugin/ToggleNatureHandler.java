ee0517fcdb8e9955e6bef8dd797aa20e5381a8be
Hagen Schink <hagen.schink@gmail.com>
2014-09-23 19:12:19 +0200

    Make SSC activation button a toggle button

--- src/org/iti/sqlschemacomparerplugin/ToggleNatureHandler.java
+++ src/org/iti/sqlschemacomparerplugin/ToggleNatureHandler.java
@@ -13,6 +13,7 @@
 package org.iti.sqlschemacomparerplugin;
 
 import java.util.Iterator;
+import java.util.Map;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
@@ -23,10 +24,14 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.commands.IElementUpdater;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.menus.UIElement;
 import org.iti.sqlschemacomparerplugin.builder.SqlSchemaComparerNature;
 
-public class ToggleNatureHandler extends AbstractHandler {
+public class ToggleNatureHandler extends AbstractHandler implements IElementUpdater {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
@@ -66,27 +71,57 @@ public class ToggleNatureHandler extends AbstractHandler {
 		try {
 			IProjectDescription description = project.getDescription();
 			String[] natures = description.getNatureIds();
-
-			for (int i = 0; i < natures.length; ++i) {
-				if (SqlSchemaComparerNature.NATURE_ID.equals(natures[i])) {
-					// Remove the nature
-					String[] newNatures = new String[natures.length - 1];
-					System.arraycopy(natures, 0, newNatures, 0, i);
-					System.arraycopy(natures, i + 1, newNatures, i,
-							natures.length - i - 1);
-					description.setNatureIds(newNatures);
-					project.setDescription(description, null);
-					return;
-				}
+			String[] newNatures = null;
+			int natureId = getNatureId(project);
+			
+			if (natureId >= 0) {
+				// Remove the nature
+				newNatures = new String[natures.length - 1];
+				System.arraycopy(natures, 0, newNatures, 0, natureId);
+				System.arraycopy(natures, natureId + 1, newNatures, natureId,
+						natures.length - natureId - 1);
+				description.setNatureIds(newNatures);
+				project.setDescription(description, null);
+			} else {
+				// Add the nature
+				newNatures = new String[natures.length + 1];
+				System.arraycopy(natures, 0, newNatures, 0, natures.length);
+				newNatures[natures.length] = SqlSchemaComparerNature.NATURE_ID;
+				description.setNatureIds(newNatures);
+				project.setDescription(description, null);
 			}
-
-			// Add the nature
-			String[] newNatures = new String[natures.length + 1];
-			System.arraycopy(natures, 0, newNatures, 0, natures.length);
-			newNatures[natures.length] = SqlSchemaComparerNature.NATURE_ID;
-			description.setNatureIds(newNatures);
-			project.setDescription(description, null);
 		} catch (CoreException e) {
 		}
+	}
+	
+	private int getNatureId(IProject project) {
+		try {
+			IProjectDescription description = project.getDescription();
+			String[] natures = description.getNatureIds();
+			
+			for (int i = 0; i < natures.length; ++i) {
+				if (SqlSchemaComparerNature.NATURE_ID.equals(natures[i])) {
+					return i;
+				}
+			}
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return -1;
+	}
+	
+	private boolean isNatureActive(IProject project) {
+		return getNatureId(project) >= 0;
+	}
+
+	@Override
+	public void updateElement(UIElement element, @SuppressWarnings("rawtypes") Map parameters) {
+		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		ISelection currentSelection = window.getSelectionService().getSelection("org.eclipse.jdt.ui.PackageExplorer");
+		IProject selectedProject = getSelectedProject(currentSelection);
+		
+		element.setChecked(isNatureActive(selectedProject));
 	}
 }
