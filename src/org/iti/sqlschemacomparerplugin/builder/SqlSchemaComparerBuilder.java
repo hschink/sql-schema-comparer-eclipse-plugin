8dec00f3b760650e3f39cbb98328a556afab6f65
Hagen Schink <hagen.schink@gmail.com>
2015-09-03 20:10:25 +0200

    Fix source code formatting

--- src/org/iti/sqlschemacomparerplugin/builder/SqlSchemaComparerBuilder.java
+++ src/org/iti/sqlschemacomparerplugin/builder/SqlSchemaComparerBuilder.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
  * Copyright (c) 10.09.2013 Hagen Schink.
- * 
+ *
  * Disclaimer: The following copyright notice does not include the method
  * checkSqlStatement(IFile, Entry<String, Integer>) because it access code
  * licensed under the terms of the GPLv3. The aforementioned method is made
  * available under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -64,7 +64,7 @@ public class SqlSchemaComparerBuilder extends IncrementalProjectBuilder {
 	class SampleDeltaVisitor implements IResourceDeltaVisitor {
 		/*
 		 * (non-Javadoc)
-		 * 
+		 *
 		 * @see org.eclipse.core.resources.IResourceDeltaVisitor#visit(org.eclipse.core.resources.IResourceDelta)
 		 */
 		public boolean visit(IResourceDelta delta) throws CoreException {
@@ -115,7 +115,7 @@ public class SqlSchemaComparerBuilder extends IncrementalProjectBuilder {
 	public static final String BUILDER_ID = "org.iti.sqlSchemaComparerPlugin.sqlSchemaComparerBuilder";
 
 	private static final String SQL_STATEMENT_MARKER_TYPE = "org.iti.sqlSchemaComparerPlugin.sqlStatementProblem";
-	
+
 	private static final String JPA_ENTITY_MARKER_TYPE = "org.iti.sqlSchemaComparerPlugin.jpaEntityDefinitionProblem";
 
 	private void addMarker(String markerType, IFile file, String message,
@@ -134,7 +134,7 @@ public class SqlSchemaComparerBuilder extends IncrementalProjectBuilder {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see org.eclipse.core.internal.events.InternalBuilder#build(int,
 	 *      java.util.Map, org.eclipse.core.runtime.IProgressMonitor)
 	 */
@@ -152,18 +152,18 @@ public class SqlSchemaComparerBuilder extends IncrementalProjectBuilder {
 		}
 		return null;
 	}
-	
+
 	void checkSqlStatements(IResource resource) {
 		if (resource instanceof IFile
 				&& resource.getName().endsWith(".java")
 				&& !ignoredFiles.contains(resource.getName())) {
 			IFile file = (IFile) resource;
-			
+
 			if (statementValidator != null) {
 				Map<String, Integer> javaStringsAndLineNumbers = ParseUtils.getAllJavaStrings(file);
-	
+
 				deleteMarkers(SQL_STATEMENT_MARKER_TYPE, file);
-				
+
 				for (Entry<String, Integer> entry : javaStringsAndLineNumbers.entrySet()) {
 					checkSqlStatement(file, entry);
 				}
@@ -176,22 +176,22 @@ public class SqlSchemaComparerBuilder extends IncrementalProjectBuilder {
 				&& resource.getName().endsWith(".java")
 				&& !ignoredFiles.contains(resource.getName())) {
 			IFile file = (IFile) resource;
-			
+
 			if (statementValidator != null) {
 				IJPASchemaFrontend frontend = new EclipseJPASchemaFrontend(file, getFormatter());
 				DirectedGraph<IStructureElement, DefaultEdge> statementSchema = frontend.createSqlSchema();
-				
+
 				if (statementSchema != null) {
 					SqlStatementExpectationValidationResult result = statementValidator.computeGraphMatching(statementSchema);
 
 					deleteMarkers(JPA_ENTITY_MARKER_TYPE, file);
-					
+
 					if (resultsAvaiable(result)) {
 						for (ISqlElement table : result.getMissingTables()) {
 							int lineNumber = ParseUtils.getLineNumber(file, ((ASTNode)table.getSourceElement()).getStartPosition());
 							addMarker(JPA_ENTITY_MARKER_TYPE, file, "Missing Table: " + table.getSqlElementId(), lineNumber, IMarker.SEVERITY_ERROR);
 						}
-						
+
 						for (ISqlElement column : result.getMissingColumns()) {
 							addColumnMarker(file, column, String.format("Missing Column: %s", column.getSqlElementId()));
 						}
@@ -240,24 +240,24 @@ public class SqlSchemaComparerBuilder extends IncrementalProjectBuilder {
 
 	private void checkSqlStatement(IFile file, Entry<String, Integer> entry) {
 		ISqlSchemaFrontend frontend = new SqlStatementFrontend(entry.getKey(), null);
-		
+
 		try {
 			DirectedGraph<IStructureElement, DefaultEdge> statementSchema = frontend.createSqlSchema();
-		
+
 			if (statementSchema != null) {
 				SqlStatementExpectationValidationResult result = statementValidator.computeGraphMatching(statementSchema);
-				
+
 				if (resultsAvaiable(result)) {
 					String message = getErrorMessage(result.getMissingTables(), "Missing Tables");
-					
+
 					if (message != "" && result.getMissingColumns().size() > 0) {
 						message += " ";
 					}
-					
+
 					message += getErrorMessage(result.getMissingColumns(), "Missing Columns");
-					
+
 					message += getErrorMessage(result.getMissingButReachableColumns(), "Missing but reachable Columns");
-	
+
 					addMarker(SQL_STATEMENT_MARKER_TYPE, file, message, entry.getValue(), IMarker.SEVERITY_ERROR);
 				}
 			}
@@ -272,13 +272,13 @@ public class SqlSchemaComparerBuilder extends IncrementalProjectBuilder {
 		if (list.size() > 0) {
 			StringBuilder message = new StringBuilder();
 			message.append(title + ": [");
-			
+
 			for (ISqlElement element : list) {
 				message.append(element.getSqlElementId() + ", ");
 			}
-			
+
 			message.setLength(message.length() - 2);
-			
+
 			return message.toString() + "]";
 		}
 
@@ -303,7 +303,7 @@ public class SqlSchemaComparerBuilder extends IncrementalProjectBuilder {
 
 			return message.toString() + "]";
 		}
-		
+
 		return "";
 	}
 
