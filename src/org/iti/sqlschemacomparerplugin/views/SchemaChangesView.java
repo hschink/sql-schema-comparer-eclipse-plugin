85f94aa5c01ee23be0dd8123c7cba99f85598997
Hagen Schink <hagen.schink@gmail.com>
2015-09-03 13:46:43 +0200

    Add SchemaChangesView stub

---
+++ src/org/iti/sqlschemacomparerplugin/views/SchemaChangesView.java
@@ -0,0 +1,17 @@
+package org.iti.sqlschemacomparerplugin.views;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.swt.widgets.Composite;
+
+public class SchemaChangesView {
+
+	@PostConstruct
+	public void createPartControl(Composite parent) {
+	}
+
+	@Focus
+	public void setFocus() {
+	}
+}
